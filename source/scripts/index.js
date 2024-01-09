f97ae7ffc25d8e857df624129cf203176253bbd5
SalemAbsolem <smirnovalexander.cat@gmail.com>
2024-01-09 19:52:45 +0300

    Создание крупных адаптивных сеток

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,6 +1,34 @@
 /* в этот файл добавляет скрипты*/
+const noJS = document.querySelectorAll('[class*="--nojs"]');
+for(let i = 0; i < noJS.length; i++) {
+  for(let j = 0; j < noJS[i].classList.length; j++) {
+    const classEl = noJS[i].classList[j].match(/--nojs/);//.input;
+    if(classEl !== null) {
+      noJS[i].classList.remove(String(classEl.input));
+    }
+  }
+}
+
+const pageCurrents = document.querySelectorAll('[class*="--current"]');
+for(let i = 0; i < pageCurrents.length; i++) {
+  pageCurrents[i].setAttribute('tabindex', '-1');
+}
+
 const menuButtonToggle = document.querySelector('.menu-btn');
+const menuBlock = document.querySelector('.navigation__list');
 
 menuButtonToggle.addEventListener('click', () => {
   menuButtonToggle.classList.toggle('menu-btn--active');
+
+  if(!menuBlock.classList.contains('navigation__list--open')) {
+    menuBlock.style.display = 'flex';
+    setTimeout(() => {
+      menuBlock.classList.add('navigation__list--open');
+    }, 100);
+  } else {
+    menuBlock.classList.remove('navigation__list--open');
+    setTimeout(() => {
+      menuBlock.style.display = '';
+    }, 300);
+  }
 });
